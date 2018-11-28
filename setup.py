77e77011d264509457a63e8ebc70fc263b2f3c97
willnx <willnx84@gmail.com>
2018-11-28 12:40:11 -0800

    bumpped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-inf-common",
       author="Nicholas Willhite, Kevin Broadware",
       author_email='willnx84@gmail.com',
-      version='2018.09.20',
+      version='2018.11.28',
       packages=find_packages(),
       include_package_data=True,
       classifiers=[
