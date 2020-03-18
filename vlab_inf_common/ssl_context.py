290a761bdbefe32f5557d987622e3c58a2a91b4b
Nicholas Willhite <willhite.nicholas@gmail.com>
2020-03-18 12:50:12 -0700

    Bug ssl (#39)
    * Better support for TLS versions when not verifying a host cert

--- vlab_inf_common/ssl_context.py
+++ vlab_inf_common/ssl_context.py
@@ -4,9 +4,8 @@ import ssl
 from vlab_inf_common.constants import const
 
 def get_context():
+    context = ssl.create_default_context()
     if const.INF_VCENTER_VERIFY_CERT is False:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        context.check_hostname = False
         context.verify_mode = ssl.CERT_NONE
-    else:
-        context = ssl.create_default_context()
     return context
